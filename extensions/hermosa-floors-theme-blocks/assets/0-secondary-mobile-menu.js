46f69caf96432effee1ccaadee563d2d9173d56e
Mitch Hijlkema <mitch@hijlkema.codes>
2024-08-21 13:55:08 +0200

    Refactor secondary mobile menu and mobile header styles

--- extensions/hermosa-floors-theme-blocks/assets/0-secondary-mobile-menu.js
+++ extensions/hermosa-floors-theme-blocks/assets/0-secondary-mobile-menu.js
@@ -1,14 +1,17 @@
 const TEMPLATE_SELECTOR = "#hc_secondary_menu";
 const MENU_SELECTOR =
   "drawer-menu drawer-menu-page[data-page-id='Menu'] .drawer-menu__content";
+const REMOVE_MENU_SELECTOR = "#hc_secondary_menu_drawer";
 
 const getMenuElement = () => document.querySelector(MENU_SELECTOR);
 const getTemplateContentNode = () =>
   document.querySelector(TEMPLATE_SELECTOR).content.cloneNode(true);
+const getElementToRemove = () => document.querySelector(REMOVE_MENU_SELECTOR);
 
 const init = () => {
   const menuElement = getMenuElement();
   const templateContentNode = getTemplateContentNode();
+  getElementToRemove()?.remove();
 
   if (menuElement) {
     menuElement.appendChild(templateContentNode);
