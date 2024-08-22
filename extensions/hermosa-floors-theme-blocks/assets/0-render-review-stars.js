bdfa0d5a40108335b7aad139c14c0ddcf8a0a17a
Mitch Hijlkema <mitch@hijlkema.codes>
2024-08-22 09:32:38 +0200

    make block scoped

--- extensions/hermosa-floors-theme-blocks/assets/0-render-review-stars.js
+++ extensions/hermosa-floors-theme-blocks/assets/0-render-review-stars.js
@@ -1,140 +1,144 @@
-/**
- * @typedef {Object} ReviewStars
- * @property {string} score - The string representation of the current score.
- * @property {number} outOf - The highest available score.
- * @property {number} stars - The amount of stars available.
- *
- * @property {boolean} hasLabel - If there should be a label rendered.
- * @property {boolean} hasAnchor - If there should be an achor label.
- *
- * @property {string} label - The HTML label associated with the review stars.
- * @property {string} target - The target element selector to replace.
- * @property {string} className - The parent classname to add to the stars.
- * @property {string} href - The href for the anchor label.
- */
-
-window.viewData = window.viewData || {};
-
-const DRAWER_SELECTOR = "drawer-menu";
-
-window.viewData.reviewStars = new (class {
+{
   /**
-   * @param {ReviewStars} config
-   * @returns {void}
+   * @typedef {Object} ReviewStars
+   * @property {string} score - The string representation of the current score.
+   * @property {number} outOf - The highest available score.
+   * @property {number} stars - The amount of stars available.
+   *
+   * @property {boolean} hasLabel - If there should be a label rendered.
+   * @property {boolean} hasAnchor - If there should be an achor label.
+   *
+   * @property {string} label - The HTML label associated with the review stars.
+   * @property {string} target - The target element selector to replace.
+   * @property {string} className - The parent classname to add to the stars.
+   * @property {string} href - The href for the anchor label.
    */
-  init(config) {
-    this.config = config;
-  }
 
-  /**
-   * The initial loop
-   */
-  loop() {
-    const targets = document.querySelectorAll(this.config.target);
-    const { hasLabel } = this.config;
+  window.viewData = window.viewData || {};
 
-    targets.forEach((target) => {
-      if (hasLabel) {
-        this.#renderStarsAndLabel(target);
-      } else {
-        this.#renderStarsOnly(target);
-      }
-    });
-  }
+  const DRAWER_SELECTOR = "drawer-menu";
 
-  /**
-   * Renders a star in place of the target element
-   * @param {HTMLElement} target
-   */
-  #renderStarsOnly(target) {
-    const { score, outOf, stars } = this.config;
-    // Create a wrapper element to replace the target.
-    const wrapperElement = document.createElement("div");
-    wrapperElement.className = target.className;
-    wrapperElement.classList.add(this.config.className);
+  window.viewData.reviewStars = new (class {
+    /**
+     * @param {ReviewStars} config
+     * @returns {void}
+     */
+    init(config) {
+      this.config = config;
+    }
 
-    // Create the star element.
-    const starElement = document.createElement("z-reviews.stars");
-    starElement.setAttribute("score", score);
-    starElement.setAttribute("out-of", outOf);
-    starElement.setAttribute("stars", stars);
+    /**
+     * The initial loop
+     */
+    loop() {
+      const targets = document.querySelectorAll(this.config.target);
+      const { hasLabel } = this.config;
 
-    // Make sure the star element is a child of the wrapper element.
-    wrapperElement.append(starElement);
+      targets.forEach((target) => {
+        if (hasLabel) {
+          this.#renderStarsAndLabel(target);
+        } else {
+          this.#renderStarsOnly(target);
+        }
+      });
+    }
 
-    // Replace the target element with the wrapper element.
-    target.replaceWith(wrapperElement);
-  }
-
-  /**
-   * Renders a star and a label in place of the target element
-   * @param {HTMLElement} target
-   */
-  #renderStarsAndLabel(target) {
-    const { score, outOf, stars, label, hasAnchor, href } = this.config;
-
-    // Create a wrapper element to replace the target.
-    const wrapperElement = document.createElement("div");
-    wrapperElement.className = target.className;
-    if (!!this.config.className)
+    /**
+     * Renders a star in place of the target element
+     * @param {HTMLElement} target
+     */
+    #renderStarsOnly(target) {
+      const { score, outOf, stars } = this.config;
+      // Create a wrapper element to replace the target.
+      const wrapperElement = document.createElement("div");
+      wrapperElement.className = target.className;
       wrapperElement.classList.add(this.config.className);
 
-    // Create the star element.
-    const starElement = document.createElement("z-reviews.stars");
-    starElement.setAttribute("rating", parseFloat(score.replace(",", ".")));
-    starElement.setAttribute("out-of", outOf);
-    starElement.setAttribute("stars", stars);
-    starElement.setAttribute("allow-fractional", "true");
+      // Create the star element.
+      const starElement = document.createElement("z-reviews.stars");
+      starElement.setAttribute("score", score);
+      starElement.setAttribute("out-of", outOf);
+      starElement.setAttribute("stars", stars);
 
-    // Create the label element
-    const labelElement = document.createElement(hasAnchor ? "a" : "p");
-    labelElement.innerHTML = label;
-    if (hasAnchor) labelElement.href = href;
+      // Make sure the star element is a child of the wrapper element.
+      wrapperElement.append(starElement);
 
-    // Make sure the star element is a child of the wrapper element.
-    wrapperElement.append(starElement, labelElement);
+      // Replace the target element with the wrapper element.
+      target.replaceWith(wrapperElement);
+    }
 
-    // Replace the target element with the wrapper element.
-    target.replaceWith(wrapperElement);
-  }
-})();
+    /**
+     * Renders a star and a label in place of the target element
+     * @param {HTMLElement} target
+     */
+    #renderStarsAndLabel(target) {
+      const { score, outOf, stars, label, hasAnchor, href } = this.config;
 
-const ownerTag = document.getElementById("review_stars_settings");
-const ownerData = {
-  display: ownerTag.getAttribute("data-display") === "true",
-  displayLabel: ownerTag.getAttribute("data-display-label") === "true",
-  displayAnchor: ownerTag.getAttribute("data-display-anchor") === "true",
+      // Create a wrapper element to replace the target.
+      const wrapperElement = document.createElement("div");
+      wrapperElement.className = target.className;
+      if (!!this.config.className)
+        wrapperElement.classList.add(this.config.className);
 
-  label: ownerTag.getAttribute("data-label"),
-  score: ownerTag.getAttribute("data-score"),
-  outOf: ownerTag.getAttribute("data-out-of"),
-  stars: ownerTag.getAttribute("data-stars"),
+      // Create the star element.
+      const starElement = document.createElement("z-reviews.stars");
+      starElement.setAttribute("rating", parseFloat(score.replace(",", ".")));
+      starElement.setAttribute("out-of", outOf);
+      starElement.setAttribute("stars", stars);
+      starElement.setAttribute("allow-fractional", "true");
 
-  className: ownerTag.getAttribute("data-classname"),
+      // Create the label element
+      const labelElement = document.createElement(hasAnchor ? "a" : "p");
+      labelElement.innerHTML = label;
+      if (hasAnchor) labelElement.href = href;
 
-  href: ownerTag.getAttribute("data-href"),
-  targetReplace: ownerTag.getAttribute("data-target-replace"),
-};
+      // Make sure the star element is a child of the wrapper element.
+      wrapperElement.append(starElement, labelElement);
 
-const handler = () => {
-  window.viewData.reviewStars.init({
-    score: ownerData.score,
-    outOf: ownerData.outOf,
-    stars: ownerData.stars,
+      // Replace the target element with the wrapper element.
+      target.replaceWith(wrapperElement);
+    }
+  })();
 
-    hasLabel: ownerData.displayLabel,
-    hasAnchor: ownerData.displayAnchor,
-    label: ownerData.label,
+  const ownerTag = document.getElementById("review_stars_settings");
+  const ownerData = {
+    display: ownerTag.getAttribute("data-display") === "true",
+    displayLabel: ownerTag.getAttribute("data-display-label") === "true",
+    displayAnchor: ownerTag.getAttribute("data-display-anchor") === "true",
 
-    target: ownerData.targetReplace,
-    className: ownerData.className,
+    label: ownerTag.getAttribute("data-label"),
+    score: ownerTag.getAttribute("data-score"),
+    outOf: ownerTag.getAttribute("data-out-of"),
+    stars: ownerTag.getAttribute("data-stars"),
+
+    className: ownerTag.getAttribute("data-classname"),
+
+    href: ownerTag.getAttribute("data-href"),
+    targetReplace: ownerTag.getAttribute("data-target-replace"),
+  };
+
+  const handler = () => {
+    window.viewData.reviewStars.init({
+      score: ownerData.score,
+      outOf: ownerData.outOf,
+      stars: ownerData.stars,
+
+      hasLabel: ownerData.displayLabel,
+      hasAnchor: ownerData.displayAnchor,
+      label: ownerData.label,
+
+      target: ownerData.targetReplace,
+      className: ownerData.className,
+    });
+
+    window.viewData.reviewStars.loop();
+  };
+
+  // Create a mutationobserver that triggers init when the drawer classList is changed
+  const observer = new MutationObserver(handler);
+  observer.observe(document.querySelector(DRAWER_SELECTOR), {
+    attributes: true,
   });
 
-  window.viewData.reviewStars.loop();
-};
-
-// Create a mutationobserver that triggers init when the drawer classList is changed
-const observer = new MutationObserver(handler);
-observer.observe(document.querySelector(DRAWER_SELECTOR), { attributes: true });
-
-handler();
+  handler();
+}
